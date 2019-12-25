560e210a3b91ff5227bcf61192553d9c900f724b
s.f.schouten <sfschouten@gmail.com>
2019-12-25 14:12:32 +0100

    make attention optional, some shape comments

--- src/egnn.py
+++ src/egnn.py
@@ -4,10 +4,15 @@ import torch.nn.functional as F
 
 class EGNN(nn.Module):
     
-    def __init__(N, F_in, F_out, P):
+    def __init__(N, F_in, F_out, P, sigma, dropout=0.6, attention=True):
         self.g = nn.Linear(F_in, F_out, bias=False)
         self.a = nn.Parameter(torch.empty(2 * F_out))
-        
+       
+        self.attention = attention
+        self.dropout = dropout
+
+        self.sigma = sigma
+
         self.N = N
         self.F_in = F_in
         self.F_out = F_out
@@ -25,12 +30,11 @@ class EGNN(nn.Module):
         """
         X nodes features [N x F_in]
         """
-        N,F = X.shape
-        X = self.g(X) # N x F_out
+        X = self.g(X)                       # N x F_out
+        N, F = X.shape
 
-        # N x N x F_out
-        X_i = X.unsqueeze(0).repeat(N,N,F)
-        X_j = X.unsqueeze(1).repeat(N,N,F)
+        X_i = X.unsqueeze(0).repeat(N,N,F)  # N x N x F_out
+        X_j = X.unsqueeze(1).repeat(N,N,F)  # N x N x F_out
 
         b = torch.cat(X_i, X_j, dim=2)      # N x N x 2*F_out
         return F.leaky_relu(b @ a).exp()    # N x N 
@@ -47,17 +51,26 @@ class EGNN(nn.Module):
         return alpha
 
 
-    def forward(self, X, E):
+    def forward(self, input):
         """
+        input = (X, E)
         X nodes features [N x F_in]
         E edge features [N x N x F_in]
-
         """
-        
+        X, E = input
+        X = F.dropout(X, p=self.dropout, training=self.training)
+
         g = self.g(X)                   # N x F_out
         a = self.attn(X, E)             # N x N x P
+        a = F.dropout(a, p=self.dropout, training=self.training)
 
-        result = torch.matmul(a, g)     # N x F_out x P  
-        result = result.view(self.N, -1)
+        if self.attention:
+            X = torch.matmul(a, g)      # N x F_out x P  
+        else:
+            X = torch.matmul(E, g)
 
-        return result
+        X = result.view(self.N, -1)
+        X = self.sigma(result)
+        return (X, a)
+
+
